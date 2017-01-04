634f27cc11d134b97e17fb3c0d12f596f4f17cb9
makun <michaelmakun@aliyun.com>
2017-01-04 23:37:03 +0800

    implement post edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,15 @@
 class PostsController < ApplicationController
 
   before_filter :authenticate_user!, :only => [:new, :create]
+  before_filter :authenticate_user!, :only => [:edit, :update, :destroy]
 
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
+  end
+
+  def edit
+    set_PostGroup_and_check_permission
   end
 
   def create
@@ -20,8 +25,35 @@ class PostsController < ApplicationController
     end
   end
 
+  def update
+
+    set_PostGroup_and_check_permission
+
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "Post Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+
+    set_PostGroup_and_check_permission
+
+    @post.destroy
+    redirect_to account_posts_path, alert: "Post Deleted"
+  end
+
 
   private
+  def set_PostGroup_and_check_permission
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+  end
+
   def post_params
     params.require(:post).permit(:content)
   end
